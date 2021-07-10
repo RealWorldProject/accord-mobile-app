aebab9c1c26b67a0e60afb779ff6a526d7aa487a
ragas17 <ragasshrestha15@gmail.com>
2021-07-11 00:01:19 +0545

    fix/future_generics: adjusted generics for asynchronous functions with Future return type

--- lib/service/storage.dart
+++ lib/service/storage.dart
@@ -3,7 +3,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class Storage {
   final tokenStorage = FlutterSecureStorage();
 
-  void storeToken(myToken) async {
+  Future<void> storeToken(myToken) async {
     await tokenStorage.write(key: 'userToken', value: myToken);
   }
 
