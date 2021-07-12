3529b246a69a8f7462bd51f4a83169e02290b463
prasanna-adhikari <55133517+prasanna-adhikari@users.noreply.github.com>
2021-07-12 19:42:00 +0545

    feature/onboard: onboarding screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:accord/screen/GetStartedScreen.dart';
+import 'package:accord/screen/LoginScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+int initScreen;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  initScreen = await prefs.getInt('initScreen');
+  await prefs.setInt('initScreen', 1);
+  // isviewed = prefs.getInt('GetStartedScreen');
   runApp(MyApp());
 }
 
@@ -10,16 +20,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily:"Poppins"
-      ),
-      debugShowCheckedModeBanner: false,
-      home: GetStartedScreen(),
-
-
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Poppins"),
+        debugShowCheckedModeBanner: false,
+        // home: isviewed != 0? GetStartedScreen():LoginScreen(),
+        initialRoute:
+            initScreen == 0 || initScreen == null ? 'onboard' : 'home',
+        routes: {
+          'home': (context) => LoginScreen(),
+          'onboard': (context) => GetStartedScreen()
+        });
   }
 }
-
