477ded6372fe64c16c07d12d4f0b08b9158a0ea9
unknown <prasannaadhikari022@gmail.com>
2021-07-23 12:00:32 +0545

    category screed added

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,4 +1,4 @@
 class Constant {
-  // static const baseURL = "http://10.0.2.2:7000/api/v1";
-  static const baseURL = "http://192.168.0.107:7000/api/v1";
+  static const baseURL = "http://10.0.2.2:7000/api/v1";
+  // static const baseURL = "http://192.168.0.107:7000/api/v1";
 }
