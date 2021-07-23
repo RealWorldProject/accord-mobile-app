a4901fa5a9339ec10b433df56979845bc34ce447
unknown <prasannaadhikari022@gmail.com>
2021-07-23 22:14:12 +0545

    feature/profile: profile page designed

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:accord/screens/post_book.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -10,24 +12,218 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Column(
-          children: [
-            TextButton(
-                child: Text("Post Book"),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => PostBook()));
-                }),
-            SizedBox(
-              height: 30,
-            ),
-            // TextButton(child: Text("Multi Image Book"), onPressed: () {
-            //   Navigator.push(
-            //       context, MaterialPageRoute(builder: (context) => MultiImage()));
-            // }),
-          ],
-        ),
+      color: Color(0xfff1f1f1),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            children: [
+              Container(
+                child: ClipPath(
+                  clipper: ArcClipper(),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height / 1.5,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/profileBG.png"),
+                        fit: BoxFit.fill,
+
+                        // image: Image.asset("assets/images/profileBG.png",height: MediaQuery.of(context).size.height,fit: BoxFit.cover,),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                child: Column(
+                  children: [
+                    Center(
+                      child: CircleAvatar(
+                        radius: 74.0,
+                        backgroundColor: Colors.white,
+                        child: CircleAvatar(
+                          radius: 70.0,
+                          backgroundImage:
+                              AssetImage("assets/images/user2.jpg"),
+                          backgroundColor: Colors.transparent,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "John Doe",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "johndoe@gmail.com",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 9,vertical: 10),
+
+                      width: MediaQuery.of(context).size.width / 1.25,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+
+                          InkWell(
+                            onTap: (){},
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.person_rounded,
+                                size: 32,
+                                color: Color(0xff0a78b2),
+                              ),
+                              title: Text(
+                                "View Profile",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xff1b98e0)),
+                              ),
+                            ),
+                          ),
+                          Divider(
+                            height: 0,
+                            thickness: 1,
+                            indent: 13,
+                            endIndent: 13,
+                            color: Color(0xffafa9a9),
+                          ),
+                          InkWell(
+                            onTap: (){},
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.favorite,
+                                size: 32,
+                                color: Colors.redAccent[700],
+                              ),
+                              title: Text(
+                                "Favourites",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xff1b98e0)),
+                              ),
+                            ),
+                          ),
+                          Divider(
+                            height: 0,
+                            thickness: 1,
+                            indent: 13,
+                            endIndent: 13,
+                            color: Color(0xffafa9a9),
+                          ),
+                          InkWell(
+                            onTap: (){},
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.menu_book_rounded,
+                                size: 32,
+                                color: Color(0xff0a78b2),
+                              ),
+                              title: Text(
+                                "My Books",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xff1b98e0)),
+                              ),
+                            ),
+                          ),
+                          Divider(
+                            height: 0,
+                            thickness: 1,
+                            indent: 13,
+                            endIndent: 13,
+                            color: Color(0xffafa9a9),
+                          ),
+
+                          InkWell(
+                            onTap: (){},
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.edit,
+                                size: 32,
+                                color: Color(0xff0a78b2),
+                              ),
+                              title: Text(
+                                "Change Password",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xff1b98e0)),
+                              ),
+                            ),
+                          ),
+                          Divider(
+                            height: 0,
+                            thickness: 1,
+                            indent: 13,
+                            endIndent: 13,
+                            color: Color(0xffafa9a9),
+                          ),
+                          InkWell(
+                            onTap: (){},
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.logout,
+                                size: 32,
+                                color: Color(0xff0a78b2),
+                              ),
+                              title: Text(
+                                "Logout",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xff1b98e0)),
+                              ),
+                            ),
+                          ),
+                          Divider(
+                            height: 0,
+                            thickness: 1,
+                            indent: 13,
+                            endIndent: 13,
+                            color: Color(0xffafa9a9),
+                          ),
+                          SizedBox(height: 20,)
+
+
+                        ],
+                      ),
+
+
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 15,
+                right: 15,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.logout,
+                    color: Colors.white,
+                  ),
+                  iconSize: 26,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
