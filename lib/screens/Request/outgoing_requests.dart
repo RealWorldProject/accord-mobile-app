92204422fbffb4c872e2a1b0ae472612133d0e6d
unknown <prasannaadhikari022@gmail.com>
2021-08-10 14:34:57 +0545

    feature/exchange: dismissable added in list

---
+++ lib/screens/Request/outgoing_requests.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+class OutgoingRequest extends StatefulWidget {
+  const OutgoingRequest({Key key}) : super(key: key);
+
+  @override
+  _OutgoingRequestState createState() => _OutgoingRequestState();
+}
+
+class _OutgoingRequestState extends State<OutgoingRequest> {
+  _outgoingBuilder() {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.symmetric(vertical: 5),
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage(
+                    "assets/images/user2.png",
+                  ),
+                  radius: 30,
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 7,
+            child: Container(
+              margin: EdgeInsets.only(top: 3),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  RichText(
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 5,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "Exchange request sent to ",
+
+                        ),
+                        TextSpan(
+                          text: "Keanu Reeves",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff13293d),
+                          ),
+                        ),
+                        TextSpan(
+                          text: " to exchange book ",
+
+                        ),
+                        TextSpan(
+                          text: "Harry Potter",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff13293d),
+                          ),
+                        ),
+                        TextSpan(
+                          text: " for ",
+
+                        ),
+                        TextSpan(
+                          text: "50 Shades of Grey.",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff13293d),
+                          ),
+                        ),
+                      ],
+                      style: const TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w600,
+
+                        color: Color(0xff606060),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 1.8,
+                  ),
+                  Text(
+                    "12 hour ago",
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff1b98e0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ClipOval(
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {},
+                child: SizedBox(
+                  width: 35,
+                  height: 35,
+                  child: Icon(
+                    Icons.more_vert,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: 8,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            // key: ObjectKey(item[index]),
+            key: UniqueKey(),
+            child: _outgoingBuilder(),
+            onDismissed: (direction) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Removed request'),
+                  action: SnackBarAction(
+                    label: "UNDO",
+                    onPressed: (){
+
+                    },
+                  ),
+                ),
+              );
+            },
+            background: Container(color:Colors.red),
+          );
+        },
+      ),
+      // child: Column(
+      //   crossAxisAlignment: CrossAxisAlignment.start,
+      //   children: [_outgoingBuilder()],
+      // ),
+    );
+  }
+}
