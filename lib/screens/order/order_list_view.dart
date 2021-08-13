6690334b20f447b18bbf2f007bc046db7caaab4a
unknown <prasannaadhikari022@gmail.com>
2021-08-13 07:33:33 +0545

    order page modified

--- lib/screens/order/order_list_view.dart
+++ lib/screens/order/order_list_view.dart
@@ -3,110 +3,158 @@ import 'package:flutter/material.dart';
 
 class OrderListView extends StatelessWidget {
   const OrderListView({Key key}) : super(key: key);
-  static const double imageHeight = 100.0;
-  static const double imageWidth = 90.0;
+  static const double imageHeight = 70.0;
+  static const double imageWidth = 65.0;
 
   _orderListView() {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 0,vertical: 5),
+      margin: EdgeInsets.symmetric(horizontal: 0, vertical: 5),
       decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(10.0)),
+          color: Colors.white, borderRadius: BorderRadius.circular(0.0)),
       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-      height: 130,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            child: Row(
-              children: [
-                Stack(
+          Text(
+            "Order 78541287412",
+            style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+                color: Colors.grey[800]),
+          ),
+          Text(
+            "Placed On 08 Mar 2021 11:35",
+            style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 12,
+                color: Colors.grey[400]),
+          ),
+          Divider(
+            height: 0,
+            thickness: 0.5,
+            indent: 0,
+            endIndent: 0,
+            color: Colors.grey[200],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Row(
                   children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: Image.network(
-                        'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR7IcZHkFtiEEEF20_gIMTSFWLqJyD70W4TQ2r1Gf71IKVn1bRb',
-                        height: imageHeight,
-                        width: imageWidth,
-                        fit: BoxFit.cover,
-                      ),
-                      // child: Image.network(
-                      //   cartItem.images,
-                      //   height: imageHeight,
-                      //   width: imageWidth,
-                      //   fit: BoxFit.cover,
-                      // ),
-                    ),
-                    Positioned.fill(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.black26,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10),
+                    Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Image.network(
+                            'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR7IcZHkFtiEEEF20_gIMTSFWLqJyD70W4TQ2r1Gf71IKVn1bRb',
+                            height: imageHeight,
+                            width: imageWidth,
+                            fit: BoxFit.cover,
                           ),
+                          // child: Image.network(
+                          //   cartItem.images,
+                          //   height: imageHeight,
+                          //   width: imageWidth,
+                          //   fit: BoxFit.cover,
+                          // ),
+                        ),
+                        Positioned.fill(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.black26,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "Godard the image book",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey[900]),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Text(
+                              "William Di Vanci",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.grey[600],
+                                  fontStyle: FontStyle.italic),
+                            ),
+                            Text(
+                              "Rs 250",
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[800],
+                              ),
+                            ),
+                            Text(
+                              ("x1"),
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w800,
+                                color: Color(0xff247BA0),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
                   ],
                 ),
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.only(left: 10, top: 10, bottom: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          "Godard the image book",
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey[900]),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        Text(
-                          "William Di Vanci",
-                          style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w100,
-                              color: Colors.grey[600],
-                              fontStyle: FontStyle.italic),
-                        ),
-                        RatingStars(
-                          4.5,
-                          20.0,
-                        ),
-                        Text(
-                          ("x1"),
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w800,
-                            color: Color(0xff247BA0),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Rs. 250",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w800,
-                          color: Color(0xff247BA0),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
+          SizedBox(height: 3,),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "1 Item,",
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey[800],
+                ),
+              ),
+              SizedBox(width: 3,),
+              Text(
+                "Total:",
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey[800],
+                ),
+              ),
+              SizedBox(width: 3,),
+              Text(
+                "Rs. 250",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff247BA0),
+
+                ),
+              ),
+
+            ],
+          )
         ],
       ),
     );
@@ -118,24 +166,7 @@ class OrderListView extends StatelessWidget {
       child: ListView.builder(
           itemCount: 8,
           itemBuilder: (context, index) {
-            return Dismissible(
-              key: UniqueKey(),
-              child: _orderListView(),
-              onDismissed: (direction) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('Removed request'),
-                    action: SnackBarAction(
-                      label: "UNDO",
-                      onPressed: (){
-
-                      },
-                    ),
-                  ),
-                );
-              },
-              background: Container(color:Colors.red),
-            );
+            return _orderListView();
           }),
     );
   }
